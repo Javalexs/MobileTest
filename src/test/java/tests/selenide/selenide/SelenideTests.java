1c9c7c7c650368d068ef31ddeb129f804dc63ba8
Alexsey Fadeev <fadeevlesha@list.ru>
2023-03-08 09:50:53 +0300

    add fromtestPfromProject

--- src/test/java/tests/selenide/selenide/SelenideTests.java
+++ src/test/java/tests/selenide/selenide/SelenideTests.java
@@ -21,68 +21,15 @@ public class SelenideTests extends TestBase {
     @Test
     @Tag("android")
     @DisplayName("Проверка текста в окне регистрации пользователя на устройстве Android")
-
-    void countryPageSearchTest() {
-
-        step("Type search", () -> {
-            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Argentina");
+    void checkTextLogInToWikipedia() {
+        step("Жмем кнопку регистрации пользователя", () -> {
+            $(byClassName("android.support.v7.widget.LinearLayoutCompat")).click();
         });
-        step("Verify content found", () -> {
-            $$(AppiumBy.className("android.widget.TextView"))
-                    .shouldHave(CollectionCondition.sizeGreaterThan(0));
-        });
-        step("Verify page is correct", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-            $(AppiumBy.className("android.webkit.WebView")).click();
-            $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Argentina"));
+        step("Проверяем наличие текста ", () -> {
+            $(id("org.wikipedia.alpha:id/explore_overflow_account_name")).shouldHave(text("Log in to Wikipedia"));
         });
     }
 
-
-    @Owner("Alexsey Fadeev")
-    @Test
-    void onBoardingStepsTest() {
-
-
-        step("Check that the text 'The Free Encyclopedia …in over 300 languages' is visible", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
-        });
-
-        step("Click on 'Сontinue'", () ->
-                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
-
-        step("Check that the text 'New ways to explore' is visible", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(text("New ways to explore"));
-        });
-
-        step("Click on 'Сontinue'", () ->
-                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
-
-        step("Check that the text 'Reading lists with sync' is visible", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(text("Reading lists with sync"));
-        });
-
-        step("Click on 'Сontinue'", () ->
-                $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
-
-        step("Check that the text 'Send anonymous data' is visible", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/primaryTextView"))
-                    .shouldHave(text("Send anonymous data"));
-        });
-    }
-
-//    void checkTextLogInToWikipedia() {
-//        step("Жмем кнопку регистрации пользователя", () -> {
-//            $(byClassName("android.support.v7.widget.LinearLayoutCompat")).click();
-//        });
-//        step("Проверяем наличие текста ", () -> {
-//            $(id("org.wikipedia.alpha:id/explore_overflow_account_name")).shouldHave(text("Log in to Wikipedia"));
-//        });
-//    }
     @Test
     @Tag("ios")
     @DisplayName("Проверка ввода электронной почты на устройстве iOS")
